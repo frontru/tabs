22d311fcbe7704c789ba8a780dd0456576efa663
frontru <85684355+frontru@users.noreply.github.com>
2022-02-08 11:04:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let tabs = document.querySelectorAll('.tabs__toggle'),
+    contents = document.querySelectorAll('.tabs__content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+        contents[index].classList.add('is-active');
+        tabs[index].classList.add('is-active');
+    });
+});
